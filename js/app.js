b7c18e9d0a7c6abb942008df8e819f41f552dab2
Siddant <siddantgurung@hotmail.co.uk>
2019-01-03 15:57:41 +0000

    testing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'hello world'
+  //const h1 = document.querySelector('h1')
+  const width = 20
+  const grid = document.querySelector('.grid')
+
+
+
+  function addElement () {
+    const newDiv = document.createElement('div')
+    grid.appendChild(newDiv)
+  }
+
+
+  //h1.textContent = 'hello world'
+  console.log(grid)
+  for(let i = 0; i<width*width; i++) {
+    addElement()
+  }
+
+
+
 })
