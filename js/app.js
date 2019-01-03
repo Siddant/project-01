fde797fd3f0b76a5505a9ddcf82a3feb559f185f
Siddant <siddantgurung@hotmail.co.uk>
2019-01-03 16:34:35 +0000

    players move

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //const h1 = document.querySelector('h1')
   const width = 20
   const grid = document.querySelector('.grid')
+  let playerIndex, previousIndex
 
 
 
@@ -10,12 +11,37 @@ document.addEventListener('DOMContentLoaded', () => {
     grid.appendChild(newDiv)
   }
 
-
   //h1.textContent = 'hello world'
-  console.log(grid)
   for(let i = 0; i<width*width; i++) {
     addElement()
   }
+
+
+  const div = document.querySelectorAll('.grid div')
+
+  playerIndex = (div.length-1)-(width/2)
+
+  div[playerIndex].classList.add('player')
+
+  function handleKeydown(e){
+    previousIndex = playerIndex
+    if(e.keyCode === 37){
+      playerIndex--
+      movePlayer(playerIndex, previousIndex)
+    }else if (e.keyCode === 39){
+      playerIndex++
+      movePlayer(playerIndex, previousIndex)
+    }
+  }
+
+  function movePlayer(playerIndex, previousIndex){
+    div[previousIndex].classList.remove('player')
+    div[playerIndex].classList.add('player')
+
+
+  }
+
+  document.addEventListener('keydown', handleKeydown)
 
 
 
